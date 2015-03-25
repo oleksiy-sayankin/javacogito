5fdb6a2356b2035dcf584b91cd1994f46ce63ab0
Oleksiy Sayankin <oleksiy.sayankin@gmail.com>
2015-03-25 17:55:31 +0200

    wiki-helper. Added addCodeTagForClassFileStructure.

--- wiki-helper/src/main/java/net/javacogito/Main.java
+++ wiki-helper/src/main/java/net/javacogito/Main.java
@@ -1,6 +1,10 @@
 package net.javacogito;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -8,9 +12,11 @@ public class Main {
         String inPlainText = Util.readFile("in.txt");
         System.out.println();
         System.out.println();
-        System.out.println( makeDoubleEnter(makeCodeTagForJAddKeywords(makeCodeTagForJavaKeywords(makeItalicJvmCommands(inPlainText)))));
+        System.out.println(makeDoubleEnter(makeCodeTagForJAddKeywords(makeCodeTagForJavaKeywords(makeItalicJvmCommands(inPlainText)))));
         System.out.println();
- //       System.out.println( replaceTabWith4Spaces(makeSpaceAtLineStart(makeItalicJvmCommands(inPlainText))));
+        System.out.println(replaceTabWith4Spaces(makeSpaceAtLineStart(makeItalicJvmCommands(inPlainText))));
+        System.out.println();
+        System.out.println(addCodeTagForClassFileStructure(inPlainText));
     }
 
     private static String makeItalicJvmCommands(String inText){
@@ -68,7 +74,105 @@ public class Main {
         String dest = "    ";
         outText = outText.replaceAll(search, dest);
         return outText;
-
     }
 
+
+
+    private static String addCodeTagForClassFileStructure(String inText){
+        String outText = inText;
+        List<String> processedEnglishWords = new ArrayList<String>();
+        while (hasUnTaggedEnglishWord(outText, processedEnglishWords)){
+            String unTaggedEnglishWord = findUnTaggedEnglishWord(outText, processedEnglishWords);
+            String search = "\\b" + unTaggedEnglishWord + "\\b";
+            String dest = Constants.START_CODE_TAG + unTaggedEnglishWord + Constants.END_CODE_TAG;
+            outText = outText.replaceAll(search, dest);
+            processedEnglishWords.add(unTaggedEnglishWord);
+        }
+        return outText;
+    }
+
+    private static boolean hasUnTaggedEnglishWord(String inText, List<String> processedEnglishWords){
+        inText = removeProcessedEnglishWord(inText, processedEnglishWords);
+        boolean result = false;
+        int index = 0;
+        int inTextLength = inText.length();
+        while(index <= inTextLength){
+            String englishWord = findEnglishWord(inText, index);
+            int englishWordIndex = findEnglishWordIndex(inText, index);
+            if (englishWordIndex == -1){
+                return false;
+            }
+            if (isTaggedEnglishWord(inText, englishWord, index)){
+                index = englishWordIndex + englishWord.length();
+            } else {
+                return true;
+            }
+        }
+        return result;
+    }
+
+    private static String findUnTaggedEnglishWord(String inText, List<String> processedEnglishWords){
+        inText = removeProcessedEnglishWord(inText, processedEnglishWords);
+        int index = 0;
+        int inTextLength = inText.length();
+        while(index <= inTextLength){
+            String englishWord = findEnglishWord(inText, index);
+            int englishWordIndex = findEnglishWordIndex(inText, index);
+            if (isTaggedEnglishWord(inText, englishWord, index)){
+                index = englishWordIndex + englishWord.length();
+            } else {
+                return englishWord;
+            }
+        }
+        return  Constants.EMPTY_STRING;
+    }
+
+    private static String findEnglishWord(String inText, int index){
+        String cutInText = inText.substring(index);
+        removeExceptions(cutInText);
+        Pattern pattern = Pattern.compile("[\\w]{2}[\\w]*");
+        Matcher matcher = pattern.matcher(cutInText);
+        if (matcher.find()) {
+            return matcher.group(0);
+        }
+        return Constants.EMPTY_STRING;
+    }
+
+    private static int findEnglishWordIndex(String inText, int index){
+        String cutInText = inText.substring(index);
+        removeExceptions(cutInText);
+        Pattern pattern = Pattern.compile("[\\w]{2}[\\w]*");
+        Matcher matcher = pattern.matcher(cutInText);
+        if (matcher.find()) {
+            return matcher.start();
+        }
+        return  -1;
+    }
+
+    private static boolean isTaggedEnglishWord(String inText, String englishWord, int index){
+        String cutInText = inText.substring(index);
+        Pattern pattern = Pattern.compile(Constants.START_CODE_TAG + englishWord + Constants.END_CODE_TAG);
+        Matcher matcher = pattern.matcher(cutInText);
+        return matcher.find();
+    }
+
+
+    private static String removeExceptions(String inText){
+        String outText = inText;
+        for(String exception : Constants.EXCEPTIONS){
+            String dest = Constants.EMPTY_STRING;
+            outText = outText.replaceAll(exception, dest);
+        }
+        return outText;
+    }
+
+    private static String removeProcessedEnglishWord(String inText, List<String> processedEnglishWords){
+        String outText = inText;
+        for(String processedEnglishWord : processedEnglishWords){
+            String source = Constants.START_CODE_TAG + processedEnglishWord + Constants.END_CODE_TAG;
+            String dest = Constants.EMPTY_STRING;
+            outText = outText.replaceAll(source, dest);
+        }
+        return outText;
+    }
 }
