6e4013890d138331052ec840c961c373bc22ea79
Oleksiy Sayankin <oleksiy.sayankin@gmail.com>
2015-04-01 19:57:16 +0300

    Added remark and table patterns

--- wiki-helper/src/main/java/net/javacogito/Main.java
+++ wiki-helper/src/main/java/net/javacogito/Main.java
@@ -12,6 +12,6 @@ public class Main {
         System.out.println();
         System.out.println(replaceTabWith4Spaces(makeSpaceAtLineStart(makeItalicJvmCommands(inPlainText))));
         System.out.println();
-        System.out.println(addCodeTagForClassFileStructure(inPlainText));
+        System.out.println(addFontCourierTagForClassFileStructure(inPlainText));
     }
 }
